933134c288ed04f3e030b49cb917da04162dd93a
Bram Waasdorp <bram@waasdorpsoekhan.nl>
2017-11-14 16:13:34 +0100

    Update ical.php

--- simple-google-i-calender-widget/includes/ical.php
+++ simple-google-i-calender-widget/includes/ical.php
@@ -38,10 +38,26 @@ class IcsParser {
                 $eventStr = substr($eventStr, 0, $endpos);
                 $e = $this->parseVevent($eventStr);
                 $events[] = $e;
-// expand repeating events
-				if (isset($e->rrule) && $e->rrule !== '') {
-					// Recurring event, parse RRULE and add appropriate duplicate events
-                $rrules = array();
+// Recurring event?
+		if (isset($e->rrule) && $e->rrule !== '') {
+/* Recurring event, parse RRULE in associative array add appropriate duplicate events
+ * only for frequencies YEARLY, MONTHLY,  WEEKLY and DAYLY
+ * frequency by multiplied by INTERVAL (default INTERVAL = 1)
+ * in a period starting after today() and after the first instance of the event, ending
+ * not after the last day of the displayed period, and not after the last instance defined by UNTIL or COUNT*Frequency*INTERVAL
+ * BY: only parse BYDAY, BYMONTH, BYMONTHDAY, possibly with multiple instances (eg BYDAY=TU,WE or BYMONTHDAY=1,2,3,4,5)
+ * not parsed: BYYEARDAY, BYSETPOS, BYHOUR, BYMINUTE, WKST
+ * examples:
+ * FREQ=MONTHLY;UNTIL=20201108T225959Z;BYMONTHDAY=8 Every 8th of the month until 20201108
+ * FREQ=MONTHLY;UNTIL=20201010T215959Z;BYDAY=2SA Monthly  2nde saturday until 20201010.
+ * FREQ=MONTHLY;BYMONTHDAY=5 Monthly the 5th
+ * FREQ=WEEKLY;INTERVAL=3;BYDAY=SU,SA Every 3 weeks on sunday and saturday
+ * FREQ=WEEKLY;COUNT=10;BYDAY=MO,TU,WE,TH,FR Every week 10 times on weekdays
+ * FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU every year last sunday of october
+ * FREQ=DAILY;COUNT=5;INTERVAL=7 Every 7 days,5 times
+
+ */
+		$rrules = array();
                 $rruleStrings = explode(';', $e->rrule);
                 foreach ($rruleStrings as $s) {
                     list($k, $v) = explode('=', $s);
@@ -64,7 +80,7 @@ class IcsParser {
                 $interval = (isset($rrules['INTERVAL']) && $rrules['INTERVAL'] !== '') ? $rrules['INTERVAL'] : 1;
              
 					/* oud
-					// FREQ=MONTHLY;UNTIL=20201108T225959Z;BYMONTHDAY=8
+					// 
 					$rrulel = explode (";", $e->rrule);
 					foreach ($rrulel as $rel) {
 						$kv = explode("=", $rel);
