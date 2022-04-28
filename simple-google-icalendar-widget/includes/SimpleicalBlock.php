ca3db07b166fa84233ae8d3b776bb8d729997cd0
ahcwa <ahcwa@bram-len.fritz.box>
2022-04-28 13:13:52 +0200

    1.6.0

--- simple-google-icalendar-widget/includes/SimpleicalBlock.php
+++ simple-google-icalendar-widget/includes/SimpleicalBlock.php
@@ -19,57 +19,57 @@ class SimpleicalBlock {
     /**
      * Front end block init
      *
-     * @see \WP_Widget::form()
+     * see \WP_Widget::form()
      *
      * @param .
      */
     public function init() {
+        echo '<!-- block_init -->';
         register_block_type( dirname(__DIR__) );
-//        wp_enqueue_style(
-//            'simple-google-icalendar-block-css', plugin_dir_url( dirname(__FILE__) ) . 'simple-google-icalendar-block.min.css',
-//            null, filemtime( plugin_dir_path( dirname(__FILE__) ) . 'simple-google-icalendar-block.min.css' )
-            );
-    }
+   }
     /**
      * Back end (admin) block init
      *
-     * @see \WP_Widget::form()
+     * see \WP_Widget::form()
      *
      * @param .
      */
     public function admin_init() {
             echo '<!-- admin_init -->';
-            wp_register_script(
-                'simple-google-icalendar-sidebar-script',
-                plugins_url( 'js/simple-google-icalendar-widget.js', dirname(__FILE__) ),
-                array(
-                    'wp-plugins',
-                    'wp-edit-post',
-                    'wp-element',
-                    'wp-components',
-                    'wp-data'
-                ),
-                filemtime( plugin_dir_path( dirname(__FILE__) ) . 'js/simple-google-icalendar-widget.js' ),
-                true,                
-                );
-            wp_register_style(
-                'simple-google-icalendar-sidebar-style',
-                plugins_url( 'css/simple-google-icalendar-sidebar.css', dirname(__FILE__) ),
-                null,
-                filemtime( plugin_dir_path( dirname(__FILE__) ) . 'css/simple-google-icalendar-sidebar.css' ),
-                );
-            register_post_meta( 'post', 'sidebar_plugin_meta_block_field', array(
-                'show_in_rest' => true,
-                'single' => true,
-                'type' => 'string',
-            ) );
+            register_block_type( dirname(__DIR__) );
+            //             wp_register_script(
+//                 'simple-google-icalendar-sidebar-script',
+// //                plugins_url( 'js/simple-google-icalendar-widget.js', dirname(__FILE__) ),
+//                 plugins_url( 'js/simple-ical-block.js', dirname(__FILE__) ),
+//                 array(
+//                     'wp-plugins',
+//                     'wp-edit-post',
+//                     'wp-element',
+//                     'wp-components',
+//                     'wp-data'
+//                 ),
+// //                filemtime( plugin_dir_path( dirname(__FILE__) ) . 'js/simple-google-icalendar-widget.js' ),
+//                 filemtime( plugin_dir_path( dirname(__FILE__) ) . 'js/simple-ical-block.js' ),
+//                 true,                
+//                 );
+//             wp_register_style(
+//                 'simple-google-icalendar-sidebar-style',
+//                 plugins_url( 'css/simple-google-icalendar-sidebar.css', dirname(__FILE__) ),
+//                 null,
+//                 filemtime( plugin_dir_path( dirname(__FILE__) ) . 'css/simple-google-icalendar-sidebar.css' ),
+//                 );
+//             register_post_meta( 'post', 'sidebar_plugin_meta_block_field', array(
+//                 'show_in_rest' => true,
+//                 'single' => true,
+//                 'type' => 'string',
+//             ) );
             
             
             
-            register_block_type( 'simple-google-icalendar-block', array(
-                'editor_script' => 'simple-google-icalendar-sidebar-script',
-                //              'editor_style'  => 'simple-google-icalendar-sidebar-css'
-            ));
+//             register_block_type( 'simple-google-icalendar-block', array(
+//                 'editor_script' => 'simple-google-icalendar-sidebar-script',
+//                 //              'editor_style'  => 'simple-google-icalendar-sidebar-css'
+//             ));
             // wp_add_inline_script(
             // 	'meow-faq-block-js',
             // 	'wp.i18n.setLocaleData( ' . json_encode( gutenberg_get_jed_locale_data( 'faq-block' ) ) . ', "faq-block" );',
@@ -82,8 +82,8 @@ class SimpleicalBlock {
             //    ) );
             load_plugin_textdomain('simple_ical', false, basename( dirname( __FILE__ ) ) . '/languages' );
     }
-    public function simple_google_icalendar_script_enqueue() {
-        wp_enqueue_script( 'simple-google-icalendar-sidebar-script' );
-        wp_enqueue_style( 'simple-google-icalendar-sidebar-style' );
-    }
+//     public function simple_google_icalendar_script_enqueue() {
+//         wp_enqueue_script( 'simple-google-icalendar-sidebar-script' );
+//         wp_enqueue_style( 'simple-google-icalendar-sidebar-style' );
+//     }
 } // end class SimpleicalBlock
