46e28944d676b9269b1fa9fa2b3c194e341ca8b2
ahcwa <ahcwa@bram-len.fritz.box>
2022-04-29 19:31:23 +0200

    1.6.0 atrributes and editable fields

--- simple-google-icalendar-widget/js/simple-ical-block.js
+++ simple-google-icalendar-widget/js/simple-ical-block.js
@@ -1,24 +1,62 @@
+/**
+ * simple-ical-block: Step 3
+ *
+ * Move styles to stylesheets - both edit and front-end.
+ * and use attributes and editable fields
+ *
+ * Note the `className` property supplied to the `edit` callback.  To use the
+ * `.wp-block-*` class for styling, plugin implementers must return an
+ * appropriate element with this class.
+ */
 ( function( blocks, i18n, element, blockEditor ) {
 	var el = element.createElement;
 	var __ = i18n.__;
 
+    var RichText = blockEditor.RichText;
 	var useBlockProps = blockEditor.useBlockProps;
 
 	blocks.registerBlockType( 'simplegoogleicalenderwidget/simple-ical-block', {
-		example: {},
+        attributes: {
+            content: {
+                type: 'array',
+                source: 'children',
+                selector: 'p',
+            },
+        },
+		example: {
+            attributes: {
+                content: 'Hello World',
+            },
+        },
 		edit: function( props ) {
+            var blockProps = useBlockProps();
+            var content = props.attributes.content;
+            function onChangeContent( newContent ) {
+                props.setAttributes( { content: newContent } );
+            }
+ 			
 			return el(
-				'p',
-				useBlockProps( { className: props.className } ),
-				__( 'Hello World, step 2 (from the editor, in green).', 'simple_ical' )
-			);
+                RichText,
+                Object.assign( blockProps, {
+                    tagName: 'p',
+                    onChange: onChangeContent,
+                    value: content,
+                } )
+            );
 		},
-		save: function() {
-			return el(
-				'p',
-				useBlockProps.save(),
-				__( 'Hello World, step 2 (from the frontend, in red).', 'simple_ical' )
-			);
+		save: function( props ) {
+            var blockProps = useBlockProps.save();
+            return el(
+                RichText.Content,
+                Object.assign( blockProps, {
+                    tagName: 'p',
+                    value: props.attributes.content,
+                } )
+            );
 		},
 	} );
-}( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor ) );
+}( window.wp.blocks,
+   window.wp.i18n,
+   window.wp.element,
+   window.wp.blockEditor )
+ );
