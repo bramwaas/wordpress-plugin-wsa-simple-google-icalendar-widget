86cab8356bc55a12ec888a693bc3083a981b8c8f
ahcwa <ahcwa@bram-len.fritz.box>
2022-04-29 14:02:17 +0200

    1.6.0 styles working

--- simple-google-icalendar-widget/js/simple-ical-block.js
+++ simple-google-icalendar-widget/js/simple-ical-block.js
@@ -1,25 +1,24 @@
-( function ( blocks, element, blockEditor ) {
-    var el = element.createElement;
- 
-    blocks.registerBlockType( 'simplegoogleicalenderwidget/simple-ical-block', {
-        edit: function (props) {
-            var blockProps = blockEditor.useBlockProps();
-            return el(
-				 'p',
-				 blockProps,
-				 'Hello World (from the editor, in green).' 
+( function( blocks, i18n, element, blockEditor ) {
+	var el = element.createElement;
+	var __ = i18n.__;
+
+	var useBlockProps = blockEditor.useBlockProps;
+
+	blocks.registerBlockType( 'simplegoogleicalenderwidget/simple-ical-block', {
+		example: {},
+		edit: function( props ) {
+			return el(
+				'p',
+				useBlockProps( { className: props.className } ),
+				__( 'Hello World, step 2 (from the editor, in green).', 'simple_ical' )
 			);
-        },
-        save: function () {
-            var blockProps = blockEditor.useBlockProps.save();
-            return el( 
-				'p', 
-				blockProps, 
-				'Hola mundo (from the frontend, in red).' 
+		},
+		save: function() {
+			return el(
+				'p',
+				useBlockProps.save(),
+				__( 'Hello World, step 2 (from the frontend, in red).', 'simple_ical' )
 			);
-        },
-    } );
-} )( window.wp.blocks,
-     window.wp.element,
-     window.wp.blockEditor
- );
+		},
+	} );
+}( window.wp.blocks, window.wp.i18n, window.wp.element, window.wp.blockEditor ) );
