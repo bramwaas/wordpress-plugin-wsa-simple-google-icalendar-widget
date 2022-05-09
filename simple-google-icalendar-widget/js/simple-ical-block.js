c5283e074fd8726152ad36cd92488e871626fcc6
ahcwa <ahcwa@bram-len.fritz.box>
2022-05-09 23:17:33 +0200

    1.6.0 set blockid = clientId (once)

--- simple-google-icalendar-widget/js/simple-ical-block.js
+++ simple-google-icalendar-widget/js/simple-ical-block.js
@@ -20,7 +20,8 @@
     var ToggleControl = components.ToggleControl;
 	blocks.registerBlockType( 'simplegoogleicalenderwidget/simple-ical-block', {
 		edit: function( props ) {
-	      { props.setAttributes( { blockid: props.attributes.__internalWidgetId  } );}
+/*	      { props.setAttributes( { blockid: props.attributes.__internalWidgetId  } );} */
+ 	      if ( '' < props.attributes.blockid ) { props.setAttributes( { blockid: props.clientId  } );} 
 			return 	el(
                'div',
                useBlockProps ({key: 'simple_ical'}),
