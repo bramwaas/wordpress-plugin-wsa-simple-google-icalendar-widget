34ae7f9b693e15deb5a2c078d5ca5483cf8d2b9f
ahcwa <ahcwa@bram-len.fritz.box>
2023-04-01 15:24:51 +0200

    2.1.1
    seems to work on first test

--- simple-google-icalendar-widget/includes/SimpleicalBlock.php
+++ simple-google-icalendar-widget/includes/SimpleicalBlock.php
@@ -149,9 +149,11 @@ class SimpleicalBlock {
                     $evdate = str_replace(array("</div><div>", "</h4><h4>", "</h5><h5>", "</h6><h6>" ), '', $evdate . wp_kses(wp_date( $dflgend, $e->end - 1) , 'post'));
                 }
                 $evdtsum = (($e->startisdate === false) ? wp_kses(wp_date( $dftsum, $e->start) . wp_date( $dftsend, $e->end), 'post') : '');
+                if ($layout < 2 && $curdate != $evdate && $curdate != '' ) {
+                    echo '</ul></li>';
+                }
                 echo '<li class="list-group-item' .  $sflgi . ((!empty($e->cal_class)) ? ' ' . sanitize_html_class($e->cal_class): '') . '">';
                 if ($layout != 2 && $curdate != $evdate ) {
-                    $curdate =  $evdate;
                     if ($layout == 3) {
                         echo '<span class="ical-date">' . ucfirst($evdate) . '</span>' . (('a' == $instance['tag_sum'] ) ? '<br>': '');}
                     else {
@@ -194,12 +196,11 @@ class SimpleicalBlock {
                 if(!empty($e->location)) {
                     echo  '<span class="location">', str_replace("\n", '<br>', wp_kses($e->location,'post')) , '</span>';
                 }
-                
-                    
                 echo '</div></li>';
-                if ($layout < 2 && $curdate != $evdate) {
-                    echo '</ul></li><!-- layout = 1 -->';
-                }
+                $curdate =  $evdate;
+            }
+            if ($layout < 2 ) {
+                echo '</ul></li>';
             }
             echo '</ul>';
             date_default_timezone_set('UTC');
