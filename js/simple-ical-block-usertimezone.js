f87f2cae94d92fc07cd1c91ea19c14f4fadd6b3d
ahcwa <ahcwa@bram-len.fritz.box>
2024-01-13 12:29:43 +0100

    2.3.0
    user client Timezone does not quite work

--- js/simple-ical-block-usertimezone.js
+++ js/simple-ical-block-usertimezone.js
@@ -3,5 +3,8 @@
  * get the user clients timezone string in a cookie to use it in php
  * v2.2.0
 */ 
+(function() {
+
   var userTimezoneString = Intl.DateTimeFormat().resolvedOptions().timeZone;
   document.cookie = "userTimezoneString=" +  userTimezoneString;
+})();
