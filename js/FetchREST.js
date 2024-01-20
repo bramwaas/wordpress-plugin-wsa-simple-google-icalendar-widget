2983ff3aac689ea90008061a7c5f518763f644ec
ahcwa <ahcwa@bram-len.fritz.box>
2024-01-20 14:22:44 +0100

    2.3.0

--- js/FetchREST.js
+++ js/FetchREST.js
@@ -41,7 +41,7 @@
 	);
 	async function requestREST(url = "", paramsObj = {}) {
 		const response = await fetch(url, {
-			method: "POST", // *GET, POST, PUT, DELETE, etc.
+			method: "POST", // *GET, POST, PUT, DELETE, OPTIONS etc.
 			mode: "cors", // no-cors, *cors, same-origin
 			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
 			credentials: "same-origin", // include, *same-origin, omit
