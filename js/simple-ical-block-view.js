0617df99e67d96431a929ae9df6f3ae7a75e3b91
ahcwa <ahcwa@bram-len.fritz.box>
2024-01-29 21:55:43 +0100

    2.3.0

--- js/simple-ical-block-view.js
+++ js/simple-ical-block-view.js
@@ -4,15 +4,16 @@
  * why this is better than plain javascript Fetch API I don't know yet.
  * v2.3.0
 */ 
-let paramsObj = {};
-const ptzid_ui = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 function getBlockByIds(paramsObj2) {
 	const fpath = "/simple-google-icalendar-widget/v1/content-by-ids";
-	const nodeList = document.querySelectorAll('[data-sib-parm]');
+	const nodeList = document.querySelectorAll('[data-sib-st]');
+	const ptzid_ui = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	let paramsObj = {};
 	for (let i = 0; i < nodeList.length; i++) {
-		paramsObj = JSON.parse(nodeList[i].getAttribute('data-sib-parm'));
 		paramsObj.tzid_ui = ptzid_ui;
+		paramsObj.blockid = nodeList[i].getAttribute('data-sib-id');
+		paramsObj.postid = nodeList[i].getAttribute('data-sib-pid');
 		console.log(paramsObj);
 		window.wp.apiFetch({
 			path: fpath,
@@ -21,11 +22,11 @@ function getBlockByIds(paramsObj2) {
 		}).then((res) => {
 			console.log(res);
 			nodeList[i].innerHTML = res.content;
-			nodeList[i].setAttribute('data-sib-id', paramsObj.blockid);
+			nodeList[i].setAttribute('data-sib-st', 'completed');
 		}
 		);
 	}
 }
 getBlockByIds(
-	paramsObj
+	{}
 );
