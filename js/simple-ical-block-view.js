f1fb030f593d7fa024f8e92aac5766ce0a683b79
ahcwa <ahcwa@bram-len.fritz.box>
2024-02-01 13:32:17 +0100

    2.3.0
    made a little bit simpler

--- js/simple-ical-block-view.js
+++ js/simple-ical-block-view.js
@@ -3,7 +3,7 @@
  * view simple-ical-block output with extra client parameter tzid_ui using REST fetched @wordpress/api-fetch 
  * why this is better than plain javascript Fetch API I don't know yet.
  * v2.3.0
-**/ 
+**/
 const endpoint = document.querySelector('link[rel="https://api.w.org/"]').href + "simple-google-icalendar-widget/v1/content-by-ids";
 let ms = Date.now();
 let fms = 0;
@@ -28,34 +28,33 @@ function getBlockByIds(paramsObj2) {
 }
 
 function fetchFromRest(dobj, ni) {
-	let mcatch = { "content": "" };
-	requestREST(endpoint, dobj).then((res) => {
+//	let mcatch = { "content": "<p>Catch</p>", "params": {} };
+
+	fetch(endpoint, {
+		method: "POST",
+		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		headers: { "Content-Type": "application/json", },
+		body: JSON.stringify(dobj),
+	}).then((response) => {
+		if (!response.ok) {
+			throw new Error(`HTTP error, status = ${response.status}`);
+		}
+		return response.json();
+	}).then((res) => {fetchOk(res);}
+	).catch((error) => {
+		ni.setAttribute('data-sib-st', 'Error :' + error.code + ':' + error.message + 'fms :' + fms);
+		ni.innerHTML = '<p>= Code: ' + error.code + '<br>= Msg: ' + error.message + '</p>' + + '<div>=Error Fms:' + fms + '</div>';
+	})
+
+	function fetchOk(res) {
 		fms = (Date.now() - ms);
 		ni.setAttribute('data-sib-st', 'completed-' + fms);
 		console.log(res);
 		rresult = res;
 		ni.innerHTML = res.content + '<div>Res fms:' + fms + '</div>';
 		//		ni.setAttribute('data-sib-st', 'completed-' + fms);
-	},
-		(error) => {
-			ni.setAttribute('data-sib-st', 'Error :' + error.code + ':' + error.message + 'fms :' + fms);
-			ni.innerHTML = '<p>= Code: ' + error.code + '<br>= Msg: ' + error.message + '</p>' + + '<div>=Error Fms:' + fms + '</div>';
-		}
-	);
-	async function requestREST(url = "", dobj = {}) {
-		try {
-			const response = await fetch(url, {
-				method: "POST",
-				cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-				headers: { "Content-Type": "application/json", },
-				body: JSON.stringify(dobj),
-			});
-			const rJson = await response.json();
-			return rJson;
-		} catch (error) { console.error("Error:", error); };
-		mcatch.content = '<h3 class="widget-title"> Error fetching data, try again </h3><p>- Catch fms:' + fms + '</p>'
-		return JSON.stringify(mcatch);
 	}
+
 
 }
 
