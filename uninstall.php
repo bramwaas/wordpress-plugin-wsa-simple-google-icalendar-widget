a56953de835af2dc797ce2706944712ff72405b9
ahcwa <ahcwa@bram-len.fritz.box>
2024-03-23 08:12:35 +0100

    2.3.0
    uninstall added to remove REST otpions after uninstall

---
+++ uninstall.php
@@ -0,0 +1,8 @@
+// if uninstall.php is not called by WordPress, die
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    die;
+}
+
+$option_name = 'simple_ical_block_attrs';
+
+delete_option( $option_name );
