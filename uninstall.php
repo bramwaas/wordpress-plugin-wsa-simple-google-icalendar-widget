79460f67a810246503a1e71d730708414b0645f0
ahcwa <ahcwa@bram-len.fritz.box>
2024-03-23 08:26:35 +0100

    2.3.0
    uninstall not working yet

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,13 @@
 // if uninstall.php is not called by WordPress, die
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+//if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
+
     die;
 }
 
 $option_name = 'simple_ical_block_attrs';
 
 delete_option( $option_name );
+
+// for site options in Multisite
+delete_site_option( $option_name );
