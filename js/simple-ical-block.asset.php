bc8f92b4d11bef9ecb5e7ef6b9f5e9d486289e71
ahcwa <ahcwa@bram-len.fritz.box>
2024-03-23 07:46:11 +0100

    2.3.0
    removed unnecessary refs

--- js/simple-ical-block.asset.php
+++ js/simple-ical-block.asset.php
@@ -2,4 +2,4 @@
 /*
  * version 2.0.0
  */
-return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-i18n', 'wp-server-side-render', 'wp-api-fetch', 'wp-data'  ), 'version' => '2.3.0-' . filemtime( plugin_dir_path( __FILE__ ) . 'simple-ical-block.js' ));
+return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-block-editor', 'wp-i18n', 'wp-server-side-render' ), 'version' => '2.3.0-' . filemtime( plugin_dir_path( __FILE__ ) . 'simple-ical-block.js' ));
