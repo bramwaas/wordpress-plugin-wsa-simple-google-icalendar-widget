27f48bd6f345676a8e9de0134b85cd6f2682a6fe
ahcwa <ahcwa@bram-len.fritz.box>
2024-03-31 13:55:12 +0200

    2.3.1
    spell error

--- includes/SimpleicalBlock.php
+++ includes/SimpleicalBlock.php
@@ -222,7 +222,7 @@ class SimpleicalBlock
                     echo $block_attributes['before_title'] . wp_kses($block_attributes['title'], 'post') . $block_attributes['after_title'];
                 }
                 self::display_block($block_attributes);
-                echo $attributes['after_widget'];
+                echo $block_attributes['after_widget'];
                 break;
             default:
                 echo "<!-- unknown wptype:" . $block_attributes['wptype'] . "-->" . PHP_EOL;
@@ -389,4 +389,29 @@ class SimpleicalBlock
         }
         return false;
     }
+    /**
+     * Close html tags in html string
+     * @params string $html String with HTML to repair
+     * @return string, rpaired HTML string
+     */
+    static function closetags($html) {
+        preg_match_all('#<(?!meta|img|br|hr|input\b)\b([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
+        $openedtags = $result[1];
+        preg_match_all('#</([a-z]+)>#iU', $html, $result);
+        $closedtags = $result[1];
+        $len_opened = count($openedtags);
+        if (count($closedtags) == $len_opened) {
+            return $html;
+        }
+        $openedtags = array_reverse($openedtags);
+        for ($i=0; $i < $len_opened; $i++) {
+            if (!in_array($openedtags[$i], $closedtags)) {
+                $html .= '</'.$openedtags[$i].'>';
+            } else {
+                unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+            }
+        }
+        return $html;
+    }
+    
 } // end class SimpleicalBlock
