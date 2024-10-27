9c6029ac5110cfbd2bd9afbc4bc5c613bfc11d4a
Bram Waasdorp <11373174+bramwaas@users.noreply.github.com>
2024-10-27 23:10:03 +0000

    2.5.0 categories in widget and working (first sight)

--- simple-google-icalendar-widget.php
+++ simple-google-icalendar-widget.php
@@ -226,6 +226,9 @@ else if ( is_wp_version_compatible( '5.9' ) )   { // block  v2
                 } else {
                     $instance['layout'] = 3;
                 }
+                $instance['categories_filter_op'] = ($new_instance['categories_filter_op']);
+                $instance['categories_filter'] = ($new_instance['categories_filter']);
+                $instance['categories_display'] = ($new_instance['categories_display']);
                 
                 $instance['event_count'] = $new_instance['event_count'];
                 if(is_numeric($new_instance['event_count']) && 0 < $new_instance['event_count']) {
@@ -364,13 +367,33 @@ else if ( is_wp_version_compatible( '5.9' ) )   { // block  v2
   			<option value="3"<?php echo ('3'==esc_attr($instance['period_limits']))?'selected':''; ?>><?php _e('Start Time of day, End Time of day'); ?></option>
   			<option value="4"<?php echo ('4'==esc_attr($instance['period_limits']))?'selected':''; ?>><?php _e('Start Whole  day, End Time of day', 'simple-google-icalendar-widget'); ?></option>
   		 </select>	
-        <p>
+        </p>
         <p>
           <label for="<?php echo $this->get_field_id('rest_utzui'); ?>"><?php _e('Use client timezone settings:', 'simple-google-icalendar-widget'); ?></label> 
           <select class="widefat" id="<?php echo $this->get_field_id('rest_utzui'); ?>" name="<?php echo $this->get_field_name('rest_utzui'); ?>" >
   			<option value=""<?php echo (''==esc_attr($instance['rest_utzui']))?'selected':''; ?>><?php _e('Use WordPress timezone settings, no REST'); ?></option>
             <option value="1"<?php echo ('1'==esc_attr($instance['rest_utzui']))?'selected':''; ?>><?php _e('Use Client timezone settings, with REST', 'simple-google-icalendar-widget'); ?></option>
-  		 </select>	
+  		 </select>
+  		</p> 	
+        <p>
+          <label for="<?php echo $this->get_field_id('categories_filter_op'); ?>"><?php _e('Categories Filter Operator:', 'simple-google-icalendar-widget'); ?></label> 
+          <select class="widefat" id="<?php echo $this->get_field_id('categories_filter_op'); ?>" name="<?php echo $this->get_field_name('categories_filter_op'); ?>" >
+  			<option value=""<?php echo (''==esc_attr($instance['categories_filter_op']))?'selected':''; ?>><?php _e('No filter'); ?></option>
+            <option value="ANY"<?php echo ('ANY'==esc_attr($instance['categories_filter_op']))?'selected':''; ?>><?php _e('ANY, one or more match', 'simple-google-icalendar-widget'); ?></option>
+            <option value="ALL"<?php echo ('ALL'==esc_attr($instance['categories_filter_op']))?'selected':''; ?>><?php _e('ALL, all match', 'simple-google-icalendar-widget'); ?></option>
+            <option value="NOTANY"<?php echo ('NOTANY'==esc_attr($instance['categories_filter_op']))?'selected':''; ?>><?php _e('NOT ANY, no match', 'simple-google-icalendar-widget'); ?></option>
+            <option value="NOTALL"<?php echo ('NOTALL'==esc_attr($instance['categories_filter_op']))?'selected':''; ?>><?php _e('NOT ALL, not all match, with REST', 'simple-google-icalendar-widget'); ?></option>
+  		 </select>
+  		</p> 	
+        <p>
+          <label for="<?php echo $this->get_field_id('categories_filter'); ?>"><?php _e('Categories Filter List:', 'simple-google-icalendar-widget'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('categories_filter'); ?>" name="<?php echo $this->get_field_name('categories_filter'); ?>" type="text" value="<?php echo esc_attr($instance['categories_filter']); ?>" />
+        </p>
+        <p>
+          <label for="<?php echo $this->get_field_id('categories_display'); ?>"><?php _e('Display categories with separator:', 'simple-google-icalendar-widget'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('categories_display'); ?>" name="<?php echo $this->get_field_name('categories_display'); ?>" type="text" value="<?php echo esc_attr($instance['categories_display']); ?>" />
+        </p>
+		<small><?php _e('Empty no display. Else display categories above event with this separator.', 'simple-google-icalendar-widget'); ?></small>
         <p>
           <label for="<?php echo $this->get_field_id('tag_sum'); ?>"><?php _e('Tag for summary:', 'simple-google-icalendar-widget'); ?></label> 
           <select class="widefat" id="<?php echo $this->get_field_id('tag_sum'); ?>" name="<?php echo $this->get_field_name('tag_sum'); ?>" >
