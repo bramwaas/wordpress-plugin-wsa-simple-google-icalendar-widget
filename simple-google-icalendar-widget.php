00c27373c326a59d951aaaf4bc92351d19448d90
Bram Waasdorp <11373174+bramwaas@users.noreply.github.com>
2026-07-16 23:22:02 +0200

    3.2.0

--- simple-google-icalendar-widget.php
+++ simple-google-icalendar-widget.php
@@ -28,7 +28,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 //define('SIB_FILE', __FILE__); //an absolute path to this directory
 define('SIB_DIR', plugin_dir_path( __FILE__ )); //an absolute path to this plugin directory
-define('SIB_TEMPLATES_DIR', SGCE_DIR .'/tmpl'); //an absolute path to the plugin templates directory
+define('SIB_TEMPLATES_DIR', SIB_DIR .'/tmpl'); //an absolute path to the plugin templates directory
 //define('SIB_DIR_URI', trailingslashit(plugins_url('',__FILE__))); //an absolute path to this directory
 define('SIB_SLUG', plugin_basename( __FILE__ )); //for updates
 
