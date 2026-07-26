ee47d1f7ae602a70306c84ede47786629ef93e70
Bram Waasdorp <11373174+bramwaas@users.noreply.github.com>
2026-07-26 22:57:07 +0200

    3.1.1

--- includes/SimpleicalHelper.php
+++ includes/SimpleicalHelper.php
@@ -22,7 +22,7 @@
  * 3.0.0 removed messages, (replaced by Notices and Warning in error_log)
  * 3.1.0 in response to PCP error replace get_block_wrapper_attributes() by expected result 
    'class="wp-block-simplegoogleicalenderwidget-simple-ical-block"'; // hardcoded untill (is_wp_version_compatible('5.6'));  
- * 3.1.3 extra widget SIB_SimpleicalWidgetNNS with no namespace as frontend for standars legacy widget SimpleicalWidget           
+ * 3.1.3 extra widget SIB_SimpleicalWidgetNNS with no namespace as frontend for standard legacy widget SimpleicalWidget           
  */
 namespace WaasdorpSoekhan\WP\Plugin\SimpleGoogleIcalendarWidget;
 // no direct access
